27372e4e6b7a342752f3c31c88472fe2c316aed0
Brandon Aaron <hello.brandon@aaron.sh>
2018-08-29 09:13:06 -0400

    update service worker

--- static/sw.js
+++ static/sw.js
@@ -20,19 +20,19 @@ workbox.precaching.precacheAndRoute([])
 //   })
 // )
 
-// workbox.routing.registerRoute(
-//   new RegExp('https://ucarecdn.com/(.*)'),
-//   workbox.strategies.cacheFirst({
-//     cacheName: 'uploadcare',
-//     plugins: [
-//       new workbox.expiration.Plugin({
-//         maxEntries: 30
-//       }),
-//       new workbox.cacheableResponse.Plugin({
-//         statuses: [0, 200]
-//       })
-//     ]
-//   })
-// )
+workbox.routing.registerRoute(
+  new RegExp('https://ucarecdn.com/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'uploadcare',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30
+      }),
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      })
+    ]
+  })
+)
 
 workbox.googleAnalytics.initialize()
