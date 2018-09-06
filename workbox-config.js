d8b73b868332d3b1ad2ada9064743ea5b4caa9b4
Brandon Aaron <hello.brandon@aaron.sh>
2018-09-06 07:35:22 -0400

    Do not precache the html
    This would attempt to precache thousands of pages in this project. Instead we should use a strategy that caches pages visited.

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   globDirectory: './public',
   globPatterns: [
-    '**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff,woff2}'
+    '**/*.{js,css,png,jpg,gif,svg,eot,ttf,woff,woff2}'
   ],
   swSrc: './static/sw.js',
   swDest: './public/sw.js'
