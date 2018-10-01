2fb48978e8d14406dd841994d5ede56d7190dd2b
Josh Taylor <josht323@gmail.com>
2018-09-30 22:30:58 -0500

    ignoring states with zero entries

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -6,10 +6,15 @@ import { stateCounts } from '../../assets/banks.js'
 const Home = () => (
   <div>
     <p>Pick a state below to see the Feeding America partner food banks.</p>
-    <ul>
-      {Object.entries(states).map(([st, name]) => <li><a href={`/${st.toLowerCase()}`}>{name}&nbsp;({stateCounts[st] || 0})</a></li>)}
-    </ul>
+    <ul>{listItems()}</ul>
   </div>
 )
+
+const listItems = () => {
+  return Object.entries(states).map(([st, name]) => {
+    if (stateCounts[st])
+      return <li><a href={`/${st.toLowerCase()}`}>{name}&nbsp;({stateCounts[st]})</a></li>
+  })
+}
 
 export default Home
