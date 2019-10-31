82dec14e39edc09db0af274301f1ffbd8ceb796f
Brandon Aaron <hello.brandon@aaron.sh>
2019-10-31 14:25:54 -0400

    Copy browserconfig.xml and robots.txt to build

--- preact.config.js
+++ preact.config.js
@@ -1,4 +1,5 @@
 const WorkboxPlugin = require('workbox-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const webpack = require('webpack')
 
 export default config => {
@@ -15,6 +16,9 @@ export default config => {
       MAPBOX_TOKEN: JSON.stringify('pk.eyJ1IjoiZnJlc2hjb2RlcyIsImEiOiJjazJlcTl6Y24wYzkwM21sNjNhejN3MjJiIn0.HWaEbpmmV4qw8SPAmo87Og')
     })
   )
+
+  config.plugins.push(new CopyWebpackPlugin([{ context: `${__dirname}/src/assets`, from: 'robots.txt' }]))
+  config.plugins.push(new CopyWebpackPlugin([{ context: `${__dirname}/src/assets`, from: 'browserconfig.xml' }]))
 
   return config
 }
