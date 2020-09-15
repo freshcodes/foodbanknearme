c182706d1d7e1608889852cc396f8d2a00d803b4
Brandon Aaron <hello.brandon@aaron.sh>
2020-09-15 14:15:18 -0400

    Dependency updates and improvements
    Includes additional meta tags and adjusts title in browser
    per route.
    
    This uninstalls the old custom service worker in favor of
    the new built-in server worker in preact cli that is
    now extendable.

--- preact.config.js
+++ preact.config.js
@@ -1,16 +1,7 @@
-const WorkboxPlugin = require('workbox-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const webpack = require('webpack')
 
 export default config => {
-  config.plugins.push(
-    new WorkboxPlugin.InjectManifest({
-      swSrc: './service-worker.js',
-      swDest: './service-worker.js',
-      include: [/\.html$/, /\.js$/, /\.svg$/, /\.css$/, /\.png$/, /\.ico$/]
-    })
-  )
-
   config.plugins.push(
     new webpack.DefinePlugin({
       MAPBOX_TOKEN: JSON.stringify('pk.eyJ1IjoiZnJlc2hjb2RlcyIsImEiOiJjazJlcTl6Y24wYzkwM21sNjNhejN3MjJiIn0.HWaEbpmmV4qw8SPAmo87Og')
